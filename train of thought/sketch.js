1e0d5e16a9a6d40c3725670c2e8da486f358aa92
neha-vasu <nv851@nyu.edu>
2020-10-20 12:56:51 -0400

    added color and more shapes and lightbulb
    frustrated by trying to get a moving background or clouds or something :(

--- train of thought/sketch.js
+++ train of thought/sketch.js
@@ -1,13 +1,52 @@
 var f = 0;
 
+
+var offset = 0;
+var strum = 1;
+var vertOne = 300;
+var vertTwo = 350;
+var v = 1;
+
 function setup() {
   createCanvas(800, 800);
   angleMode(DEGREES);
-  background(255);
+  background('#8ed6ef');
 }
 
 function draw() {
+ 
 
+//   background(220);
+//   stroke(4);
+//   fill(0);
+//   noStroke();
+//   beginShape();
+//   vertex(0, height);
+//   for(var x = 0; x < width; x++){
+//     //var angle = map(x, 0, width, 0, TWO_PI);
+//     var angle = offset + x * 0.01;
+//     // map x between 0 and width to 0 and Two Pi
+//     var y = map(degrees(sin(angle)), -strum, strum, vertOne, vertTwo);
+//     vertex(x, y);
+//   }
+//   vertex(width, height);
+//   endShape();
+//   offset += 0.1;
+//   if (vertOne <=5){
+//      v = -1;
+//      vertOne -= 0.5*v;
+//      vertTwo -= 0.5*v;
+//   } else if (vertOne >= width && v===-1){
+//     v = 1;
+//     vertOne -= 0.5*v;
+//     vertTwo -= 0.5*v;
+//   } else 
+//     {
+//     vertOne -= 0.5*v;
+//     vertTwo -= 0.5*v;
+//    }
+  
+  
 //   clear();
 //   f += 0.002
 //   for (j = 1; j < 6; j++) {
@@ -20,9 +59,15 @@ function draw() {
 //   }
   push()
 
-  lightbulbDraw();
-  // translate(0, 100)
-  // scale(0.9)
+
+  translate(50, 100)
+  scale(0.85)
+  
+  push()
+  translate (-110, -30)
+  scale (1.2)
+   lightbulbDraw();
+  pop()
 
   // push()
   // translate(-290, -430)
@@ -53,6 +98,8 @@ function draw() {
   translate(-40, 400)
   trainDraw();
   pop()
+  
+   
 
   pop()
 
@@ -65,72 +112,141 @@ function draw() {
 
 function lightbulbDraw()
 {
+  // angleMode (DEGREES)
+  noStroke()
+   var lightBlue = color ('#256eaf')
+  var lightestBlue = color('#8ed6ef')
+var darkBlue = color ('#1d3475')
+
+  fill('#ffea55') //bright yellow
   circle (595, 320, 140);
-  quad ()
+  quad (525, 330, 665, 330,630, 440, 555, 440)
+  
+  fill (darkBlue)
+  circle (592, 480, 80)
+  
+  strokeWeight(3)
+  stroke(darkBlue)
+  line (560, 345, 580, 445)
+  line(625, 345, 605, 445 )
+  
+  noFill()
+  
+arc (570, 345, 22, 20, 0, 180)
+  arc (592, 345, 22, 20, 0, 180)
+  arc (614, 345, 22, 20, 0, 180)
+  
+  noStroke()
+  fill(lightestBlue)
+    rect (552, 440, 80, 50, 5)
+  fill(lightBlue)
+  rect (543, 440, 100, 10, 5)
+   rect (543, 455, 100, 10, 5)
+  rect (543, 470, 100, 10, 5)
+  
+  
+  
+  
 }
 
 function trainDraw() {
   strokeWeight(2);
   stroke(0);
   fill(255);
+  noStroke()
 
-  //lines connecting train compartments 
+ var lightPink = color ('#f4bae5')
+  var lightBlue = color ('#256eaf')
+  var darkBlue = color ('#1d3475')
+
+  //lines connecting train compartments
+  fill (darkBlue)// dark blue
   rect(200, 195, 590, 10)
 
 
   //chimney smoke thing
+  fill (lightBlue)
   rect(150, 40, 40, 100)
-  rect(140, 20, 60, 20)
-
+   fill (darkBlue)// dark blue
+  rect(140, 20, 60, 20, 10)
+fill (255)
 
 
 
   //the bottom of train front 
-  triangle(80, 245, 100, 195, 195, 195)
-  triangle(100, 195, 100, 245, 80, 245)
+  // triangle(80, 245, 100, 195, 195, 195)
+    fill (lightBlue)
+  triangle(100, 195, 120, 245, 80, 245)
 
   //train front 
+  fill (lightPink)
   rect(100, 100, 220, 100)
 
+  fill (lightBlue)
   rect(100, 145, 290, 10)
-  ellipse(100, 148, 40, 95)
+  
+  fill (darkBlue)// dark blue
+  ellipse(100, 148, 40, 105)
+  fill (255)
 
   //rectangle train front window compartment
 
+  fill (lightBlue)
   rect(280, 20, 150, 200)
-  rect(100, 195, 330, 50)
-  rect(265, -20, 180, 40)
-  rect(302, 40, 105, 105)
+  rect(95, 195, 335, 50, 20)
+  fill (darkBlue)// dark blue
+  rect(265, -20, 180, 40, 20)
 
+   fill('rgba(244, 186, 229, 2)')// light pink
+
+  rect(302, 40, 105, 105, 10)
+fill (255)
 
   //wheels of first compartment
 
+  fill (darkBlue)// dark blue
   circle(150, 245, 80)
+   fill(lightPink)// light pink
   circle(150, 245, 30)
 
-  circle(260, 245, 80)
-  circle(260, 245, 30)
+  fill (darkBlue)// dark blue
+  circle(265, 245, 80)
+   fill(lightPink)// light pink
+  circle(265, 245, 30)
 
+  fill (darkBlue)// dark blue
   circle(380, 245, 80)
+  fill(lightPink)// light pink
   circle(380, 245, 30)
 
+  fill(255)
   // base of second compartment
-  rect(470, 195, 330, 50)
+   fill (lightBlue)
+  rect(465, 195, 340, 50, 15)
 
   //wheels of second compartment
 
+  fill (darkBlue)// dark blue
   circle(520, 245, 80)
+  fill(lightPink)// light pink
   circle(520, 245, 30)
 
-  circle(630, 245, 80)
-  circle(630, 245, 30)
+  fill (darkBlue)// dark blue
+  circle(635, 245, 80)
+  fill(lightPink)// light pink
+  circle(635, 245, 30)
 
+  fill (darkBlue)// dark blue
   circle(750, 245, 80)
+  fill(lightPink)// light pink
   circle(750, 245, 30)
+  fill (255)
 
   //second compartment
-  rect(470, 95, 330, 100)
-  rect(460, 75, 350, 20)
+  fill('rgba(244, 186, 229, 0.5)')
+  rect(470, 105, 330, 90)
+ fill('rgba(29, 52, 117, 0.5)')
+  rect(460, 65, 350, 40, 20)
 
   // triangle ()
   //   rect ()
@@ -141,6 +257,7 @@ function trainDraw() {
 function brainDraw() {
   noStroke();
   fill('#eaade4')
+  fill ('#e5aac9')
   // arc (300, 300, 40, 40, 0, 40);
   circle(300, 305, 30)
   circle(280, 310, 35)
